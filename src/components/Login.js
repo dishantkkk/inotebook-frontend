fc3674bdac20f800c2eedf59fc3768f29ea29350
Dishant Kumar Kushwaha <dishantkushwaha71@gmail.com>
2023-03-01 15:12:12 +0530

    New Commit

---
+++ src/components/Login.js
@@ -0,0 +1,81 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+export const Login = (props) => {
+  const [credentials, setCredentials] = useState({ email: "", password: "" });
+  let navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await fetch("http://localhost:5000/api/auth/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: credentials.email,
+        password: credentials.password,
+      }),
+    });
+
+    const json = await response.json();
+    console.log("response " + json);
+
+    if (json.success) {
+      // Save the auth token and redirect
+      localStorage.setItem("token", json.authToken);
+      props.showAlert("Login Successfully", "success");
+      navigate("/");
+    } else {
+      props.showAlert("Invalid credentials", "danger");
+    }
+  };
+
+  const onChange = (e) => {
+    setCredentials({ ...credentials, [e.target.name]: e.target.value });
+  };
+
+  return (
+    <div className="container">
+      <h2 className="text-warning my-2 mb-4">Login to Continue to iNotebook</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">
+            Email address
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            value={credentials.email}
+            onChange={onChange}
+            id="email"
+            name="email"
+            aria-describedby="emailHelp"
+          />
+          <div id="emailHelp" className="form-text">
+            We'll never share your email with anyone else.
+          </div>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            value={credentials.password}
+            onChange={onChange}
+            name="password"
+            id="password"
+          />
+        </div>
+
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
